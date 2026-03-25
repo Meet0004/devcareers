51b8d85201e2b748997dee14c13fa88ada78a183
Meet0004 <meetgssoni04@gmail.com>
2026-03-26 03:09:50 +0530

    added new package

--- frontend/src/data/resourceData/packagesData.js
+++ frontend/src/data/resourceData/packagesData.js
@@ -7,10 +7,10 @@ const packagesData = [
     description: 'Everything you need to ace technical interviews',
     includedResourceIds: [3, 14, 32], // IDs of resources included
     originalPrice: 67,
-    discountedPrice: 1,
+    discountedPrice: 39,
     // driveLink: 'https://drive.google.com/drive/folders/1eKkVa3WcA4qF9PBRc0vO3xztmycRcgL6?usp=sharing',
     link:'https://topmate.io/meet_g/1969991 ',
-    savings: 26,
+    savings: 28,
     expDate: '20-04-2026'
   }
   , {
@@ -21,18 +21,29 @@ const packagesData = [
     originalPrice: 48,
     discountedPrice: 29,
     link: 'https://topmate.io/meet_g/1980496',
-    savings: 39,
+    savings: 19,
     expDate: '27-03-2026',
   }
   , {
     id: 'pkg-3',
     title: 'Ultimate Developer Interview Question',
     description: '400+ curated interview questions with answers across Node.js, React, JavaScript, Java, and SQL - perfect for complete tech interview preparation.',
-    includedResourceIds: [30, 20, 22, 31, 1],
+    includedResourceIds: [30, 20, 22, 31, 1, 2],
     originalPrice: 75,
     discountedPrice: 49,
     link: 'https://topmate.io/meet_g/1980309',
     savings: 34,
+    expDate: '27-03-2026',
+  }
+  ,{
+    id: 'pkg-4',
+    title:'Ultimate DSA + Technical Round Preparation Bundle',
+    description:'15+ top company LeetCode sheets, 100+ structured DSA questions, 400+ curated interview questions with answers across Node.js, React, JavaScript, Java, and SQL - perfect for complete tech interview preparation.',
+    includedResourceIds: [28, 30, 20,7,12,22, 31, 1, 2],
+    originalPrice: 123,
+    discountedPrice: 79,
+    link: 'https://topmate.io/meet_g/1980309',
+    savings: 44,
     expDate: '27-03-2026',
   }
 ];

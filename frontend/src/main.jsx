601d620bb9196ae8615d5994ab61ebf0893ca58c
Meet0004 <meetgssoni04@gmail.com>
2026-05-10 15:12:09 +0530

    updated blogs title and added vercel analytics

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,15 @@
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom'
 import { Analytics } from '@vercel/analytics/react'
 
 createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <BrowserRouter>
-        <App />
-        <Analytics />
+      <App />
+      <Analytics />
     </BrowserRouter>
+  </React.StrictMode>
 )
